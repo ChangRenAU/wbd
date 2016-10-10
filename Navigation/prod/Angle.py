b20a42980957aab940f7ad2e4750e36389c12483
czr0049 <czr0049@auburn.edu>
2016-10-09 21:55:52 -0500

    Tested CA01 Commit

--- Navigation/prod/Angle.py
+++ Navigation/prod/Angle.py
@@ -1,94 +1,128 @@
-import re
 class Angle():
 
-    def __init__(self,angle=0):
-        self.angle = angle
+    def __init__(self):
+        self.angle = 0.0
+        self.className = "Angle."
             
     def setDegrees(self, degrees=0):
+        methodName = "setDegrees:  "
         
         if(not(isinstance(degrees,(int,float)))):
-            raise ValueError("This degrees should be an int or float.")
+            raise ValueError(self.className + methodName + "invalid degrees")
         try:
             temp=float(degrees)  
-            self.angle = round(temp%360.0,1)
+            self.angle = temp%360.0
             return self.angle
                 
         except ValueError:
             print("ValueError!!!")
                   
-    def setDegreesAndMinutes(self, angleString=None):
+    def setDegreesAndMinutes(self, degrees=None):
+        methodName="setDegreesAndMinutes:  "
+
+        if(degrees == None):
+            raise ValueError(self.className + methodName + "invalid angle")
+        if(not(isinstance(degrees,str))):
+            raise ValueError(self.className + methodName + "invalid angle")
        
         try:
-            
-            nums=str(angleString).split('d')
+            tag = degrees.index('d')
+            if(tag == 0):
+                raise ValueError
+                       
+            nums=str(degrees).split('d')
             start,end = map(float,nums)
             
-            #check to see if the start is 
-            if not re.search(r'\.',str(start)):
-                raise ValueError("ValueError!")
+            #degrees should be int 
+            tagStart = str(start).find('.0')
+            if(tagStart == -1):
+                raise ValueError
+            #minutes should greater than 0
+            if("-" in str(end)):
+                raise ValueError
             
-            if start > 0:
-                self.angle = round((start%360 + end/60.0)%360,1)
-                return self.angle
+            #minutes must have only one decimal place     
+            tagEnd = str(end).find(".")
+            if(tagEnd != -1):
+                if((tagEnd == 0) or (len(str(end))-tagEnd > 2)):
+                    raise ValueError
             
-            elif start < 0 and (start%360 - end/60) > 0:
-                self.angle = round((start%360 - end/60.0),1)
-                return self.angle
-            
-            elif start < 0 and (start%360 - end/60) < 0:
-                self.angle = round((start%360 - end/60 + 360.0),1)
-                return self.angle
+            if start >= 0.0:
+                self.angle = float((start%360.0 + end/60.0)%360.0)
                 
-            else:
-                raise ValueError("Value Error.Input error.")
-                
-        except ValueError:
-            print("Value Error.Input error.")
-   
-   
-    def add(self, angle):
-       
-        try:
+            elif start < 0.0 and (start%360.0 - end/60.0) > 0.0:
+                self.angle = float(start%360.0 - end/60.0)
             
-            self.angle += float(angle)
+            elif start < 0.0 and (start%360.0 - end/60.0) < 0.0:
+                self.angle = float(start%360.0 - end/60.0 + 360.0)
+                
+        except:
+            raise ValueError(self.className + methodName + "invalid angle")
+        
+        return self.angle
+      
+    def add(self, angle = None):
+        methodName = "add:  "
+        if(angle == None):
+            raise ValueError(self.className + methodName + "invalid angle")
+        if(not(isinstance(angle,Angle))):
+            raise ValueError(self.className + methodName + "invalid angle")
+        try:            
+            self.angle += angle.getDegrees()
+            temp=float(self.angle)  
+            self.angle = temp%360.0
             return self.angle
         
-        except ValueError:
-            print("Input error, ValueError!!!")
+        except:
+            raise ValueError(self.className + methodName + "ValueError")
     
-    def subtract(self, angle):
+    def subtract(self, angle = None):
+        methodName = "subtract:  "
+        if(angle == None):
+            raise ValueError(self.className + methodName + "invalid angle")
+        if(not(isinstance(angle,Angle))):
+            raise ValueError(self.className + methodName + "invalid angle")
         
         try:
-
-            self.angle -= float(angle)
+            self.angle -= angle.getDegrees()
+            temp=float(self.angle)  
+            self.angle = temp%360.0
             return self.angle
         
-        except ValueError:
-            print("Input error, ValueError!!!")
-            
-    
-    def compare(self, angle):
-
-        try:
-            temp = float(angle)
-            if self.angle>temp:
-                return 1
-            elif self.angle<temp:
-                return -1
-            else:
-                return 0
-            
-        except ValueError:
-            print("Input error, ValueError!!!")
-
+        except:
+            raise ValueError(self.className + methodName + "ValueError")
+                
+    def compare(self, angle = None):
+        methodName = "compare:  "
+        if(angle == None):
+            raise ValueError(self.className + methodName + "invalid angle")
+        if(not(isinstance(angle,Angle))):
+            raise ValueError(self.className + methodName + "invalid angle")
+        
+        if (self.angle > angle.getDegrees()):
+            return 1
+        elif (self.angle < angle.getDegrees()):
+            return -1
+        else:
+            return 0
     
     def getString(self):
         
-        self.start = int(self.angle)
-        self.end = (self.angle - self.start)*60
-        return   str(self.start)+'d'+str(self.end)
-    
+        temp = self.getDegrees()
+        self.start = int(temp)
+        self.end = round((temp - self.start)*60,1)
+        wholestring = str(self.start)+'d'+str(self.end)
+        return wholestring    
     
     def getDegrees(self):
-              
-        return self.angle
+        
+        temp = self.angle
+        degree= int(temp)
+        if(degree == 0):
+            tempMinute = temp
+        else:
+            tempMinute = temp % degree
+        minute = round(tempMinute*60,1)/60
+        temp = degree + minute
+        return temp
+
